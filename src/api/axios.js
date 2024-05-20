fd84ecc14a30ec3863d3265a1d6acbe3df9b7f15
John Angel Henao <john.alvarez@correounivalle.edu.co>
2024-05-20 13:58:51 -0500

    baseURL

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,13 @@
-// Define la URL base de la API utilizando una variable de entorno
-const baseURL = process.env.REACT_APP_API_URL || "http://localhost:3000/api/v1";
+import axios from "axios";
+
+// Define la URL base de la API para entorno local
+const localBaseUrl = "http://localhost:3000/api/v1";
+
+// Define la URL base de la API para entorno de producción
+const productionBaseUrl = "https://api-devtest-jah.vercel.app/api/v1";
+
+// Obtén la URL base de la API dependiendo del entorno
+const baseURL = window.location.hostname === 'localhost' ? localBaseUrl : productionBaseUrl;
 
 // Crea una instancia de Axios con la URL base configurada
 const instance = axios.create({
